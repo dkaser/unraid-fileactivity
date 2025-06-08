8a998e293e039b2499a640046c00f74cff9c8c45
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-06-08 04:55:26 +0000

    feat: tables

--- src/usr/local/emhttp/plugins/file.activity/include/Pages/DiskActivity.php
+++ src/usr/local/emhttp/plugins/file.activity/include/Pages/DiskActivity.php
@@ -5,8 +5,10 @@ namespace EDACerton\FileActivity;
 use EDACerton\PluginUtils\Translator;
 
 /*
-    Copyright (C) 2017-2025, Dan Landon
-    Copyright (C) 2025  Derek Kaser
+    Copyright 2015-2016, Lime Technology
+    Copyright 2015-2016, Bergware International.
+    Copyright 2015-2025, Dan Landon
+    Copyright 2025  Derek Kaser
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -27,71 +29,141 @@ if ( ! defined(__NAMESPACE__ . '\PLUGIN_ROOT') || ! defined(__NAMESPACE__ . '\PL
 }
 
 $tr = $tr ?? new Translator(PLUGIN_ROOT);
-?>
 
-<table class="tablesorter shift ups">
-<thead><tr><th><?= $tr->tr("disk_activity"); ?></th></tr></thead>
+?>
+<link type="text/css" rel="stylesheet" href="/plugins/file.activity/assets/style.css">
+
+<link href="/plugins/file.activity/assets/select2.min.css" rel="stylesheet">
+<script src="/plugins/file.activity/assets/select2.min.js"></script>
+<link href="/plugins/file.activity/assets/datatables.min.css" rel="stylesheet">
+<script src="/plugins/file.activity/assets/datatables.min.js"></script>
+
+<table id='diskTable'>
+    <thead>
+        <tr>
+            <th><strong><?= $tr->tr("date"); ?></strong></th>
+            <th><strong><?= $tr->tr("action"); ?></strong></th>
+            <th><strong><?= $tr->tr("file_path"); ?></strong></th>
+            <th>Test</th>
+        </tr>
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+    <tfoot>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+    </tfoot>
 </table>
 
-<br>
-<?php
-echo ($resize ?? false) ? "<pre class='up' style='display:none'>" : "<pre class='up'>";
-
-$display_events = ((isset($file_activity_cfg['DISPLAY_EVENTS'])) && ($file_activity_cfg['DISPLAY_EVENTS'])) ? $file_activity_cfg['DISPLAY_EVENTS'] : "25";
-$filesactive    = "";
-
-// Disk activity.
-$disks = glob("/mnt/disk[^s]*") ?: array();
-natcasesort($disks);
-foreach ($disks as $disk) {
-    $disk_dev = $disk . "/";
-    $files    = shell_exec("cat /var/log/file.activity.log 2>/dev/null | grep " . escapeshellarg($disk_dev) . " | tail -n " . escapeshellarg($display_events));
-    if ( ! empty($files)) {
-        $filesactive .= "<strong>** Disk " . substr($disk, 9) . " **</strong>\n";
-        $filesactive .= $files;
-        $filesactive .= "\n";
-    }
-}
-
-// UD disks activity.
-$files = shell_exec("cat /var/log/file.activity.log 2>/dev/null | grep '/mnt/disks/' | tail -n " . escapeshellarg($display_events));
-if ( ! empty($files)) {
-    $filesactive .= "<strong>** Unassigned Devices **</strong>\n";
-    $filesactive .= $files;
-    $filesactive .= "\n";
-}
-
-// Cache and pools activity.
-foreach (($pools ?? array()) as $pool) {
-    $files = shell_exec("cat /var/log/file.activity.log 2>/dev/null | grep '/mnt/" . $pool . "' | tail -n " . escapeshellarg($display_events));
-    if ( ! empty($files)) {
-        $filesactive .= "<strong>** " . ucfirst($pool) . " **</strong>\n";
-        $filesactive .= $files;
-        $filesactive .= "\n";
-    }
-}
-echo $filesactive;
-echo "</pre>";
-?>
-
 <script>
-<?if ($resize ?? false) { ?>
-$(function() {
-  $('pre.up').css('height',Math.max(window.innerHeight-400,370)).show();
-});
-<?}?>
+$(document).ready( function () {
+    $('#diskTable').DataTable({
+        ajax: {
+            url: '/plugins/file.activity/data.php/disk',
+            dataSrc: ''
+        },
+        columns: [
+            { name: "Timestamp", data: 'timestamp' },
+            { name: "action", data: 'action',
+                orderable: false
+             },
+            { name: "path", data: 'filePath' },
+            { name: "disk", data: 'disk', visible: false }
+        ],
+        paging: false,
+        ordering: false,
+        rowGroup: {
+            dataSrc: 'disk'
+        },
+        layout: {
+            topStart: {
+                buttons: [
+                    {
+                        text: '<?= $tr->tr("refresh"); ?>',
+                        action: function ( e, dt, node, config ) {
+                            dt.ajax.reload();
+                        }
+                    }
+                ]
+            }
+        },
+        initComplete: function () {
+            this.api()
+                .columns([1])
+                .every(function () {
+                    var title = this.header();
+                    //replace spaces with dashes
+                    title = $(title).html().replace(/[\W]/g, '-');
+                    title = title + '-disk';
+                    var column = this;
+                    var select = $('<select id="' + title + '" class="select2" ></select>')
+                       .appendTo($("#diskTable thead tr:eq(1) th").eq(column.index()).empty())
+                        .on( 'change', function () {
+                        //Get the "text" property from each selected data 
+                        //regex escape the value and store in array
+                        var data = $.map( $(this).select2('data'), function( value, key ) {
+                            return value.text ? '^' + $.fn.dataTable.util.escapeRegex(value.text) + '$' : null;
+                                    });
+                        
+                        //if no data selected use ""
+                        if (data.length === 0) {
+                            data = [""];
+                        }
+                        
+                        //join array into string with regex or (|)
+                        var val = data.join('|');
+                        
+                        //search for the option(s) selected
+                        column
+                                .search( val ? val : '', true, false )
+                                .draw();
+                        } );
+    
+                    column.data().unique().sort().each( function ( d, j ) {
+                        select.append( '<option value="'+d+'">'+d+'</option>' );
+                    } );
+                
+                //use column title as selector and placeholder
+                $('#' + title).select2({
+                    multiple: true,
+                    closeOnSelect: true,
+                    width: '166px',
+                });
+                
+                //initially clear select otherwise first option is selected
+                $('#' + title).val(null).trigger('change');
+                });
+
+            this.api()
+                .columns([2])
+                .every(function () {
+                    let column = this;
+                    let title = column.footer().textContent;
+    
+                    // Create input element
+                    let input = document.createElement('input');
+                    input.placeholder = title;
+                    $("#diskTable thead tr:eq(1) th").eq(column.index()).empty().append(input);
+    
+                    // Event listener for user input
+                    input.addEventListener('keyup', () => {
+                        if (column.search() !== this.value) {
+                            column.search(input.value).draw();
+                        }
+                    });
+                });
+        }
+    });
+} );
 </script>
 
-<div style="position:relative;float:left;text-align:right;margin-bottom:24px">
-	<input type="button" value="<?= $tr->tr("refresh"); ?>" title="<?= $tr->tr("refresh_page"); ?>" onclick="refresh()">
-</div>
-<div style="position:relative;float:left;text-align:right;margin-bottom:24px">
-	<form name="clear log" method="POST" action="/update.php" target="progressFrame">
-		<input type="hidden" name="#command" value="/plugins/file.activity/scripts/rc.file.activity">
-		<input type="hidden" name="#arg[1]" value="clear">
-		<input type="submit" value="<?= $tr->tr("clear"); ?>" title="<?= $tr->tr("clear_log"); ?>">
-	</form>
-</div>
-<div style="position:relative;float:left;text-align:right;margin-bottom:24px">
-	<input type="button" value="<?= $tr->tr("done"); ?>" onclick="done()">
-</div>
---
+++ src/usr/local/php/unraid-fileactivity/unraid-fileactivity/Activity.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace EDACerton\FileActivity;
+
+use EDACerton\PluginUtils\Utils;
+
+/*
+    Copyright (C) 2025  Derek Kaser
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+class Activity
+{
+    private int $display_events;
+
+    public function __construct()
+    {
+        // Constructor can be used for initialization if needed.
+        $file_activity_cfg    = Utils::parse_plugin_cfg('file.activity');
+        $this->display_events = ((isset($file_activity_cfg['DISPLAY_EVENTS'])) && (is_numeric($file_activity_cfg['DISPLAY_EVENTS']))) ? intval($file_activity_cfg['DISPLAY_EVENTS']) : 25;
+    }
+    /**
+     * @return array<string, list<array<string, string>>>
+     */
+    public function getShareActivity(): array
+    {
+        $result = array();
+
+        // Share activity.
+        $shares = $this->getShares();
+        natcasesort($shares);
+        foreach ($shares as $share) {
+            $share_dev = basename($share) . "/";
+            $files     = $this->getActivityEntries("/mnt/disk\d+/{$share_dev}", $this->display_events);
+            if ( ! empty($files)) {
+                $result[basename($share)] = $files;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array<string, list<array<string, string>>>
+     */
+    public function getDiskActivity(): array
+    {
+        $result = array();
+
+        // Share activity.
+        $disks = $this->getDisks();
+        foreach ($disks["array"] as $disk) {
+            $dev   = basename($disk) . "/";
+            $files = $this->getActivityEntries("/mnt/{$disk}/", $this->display_events);
+            if ( ! empty($files)) {
+                $result[$disk] = $files;
+            }
+        }
+
+        $files = $this->getActivityEntries("/mnt/disks/", $this->display_events);
+        if ( ! empty($files)) {
+            $result["Unassigned Devices"] = $files;
+        }
+
+        foreach ($disks['pool'] as $pool) {
+            $files = $this->getActivityEntries("/mnt/{$pool}/", $this->display_events);
+            if ( ! empty($files)) {
+                $result[ucfirst($pool)] = $files;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function getShares(): array
+    {
+        $shares = parse_ini_file("/var/local/emhttp/shares.ini", true) ?: array();
+        return array_keys($shares);
+    }
+
+    /**
+     * @return array<string, list<string>>
+     */
+    public function getDisks(): array
+    {
+        $result = [
+            "array" => [],
+            "pool"  => [],
+        ];
+
+        $disks = parse_ini_file("/var/local/emhttp/disks.ini", true) ?: array();
+
+        foreach ($disks as $disk => $info) {
+            if (isset($info['type']) && $info['type'] === 'Data') {
+                $result['array'][] = $disk;
+            } elseif (isset($info['type']) && $info['type'] === 'Cache') {
+                if (isset($info['fsType']) && ! empty($info['fsType'])) {
+                    $result['pool'][] = $disk;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return list<array<string, string>>
+     */
+    private function getActivityEntries(string $disk, int $display_events): array
+    {
+        $files      = shell_exec("cat /var/log/file.activity.log 2>/dev/null | grep -P " . escapeshellarg($disk) . " | tail -n " . strval($display_events));
+        $filesArray = array();
+
+        if ($files) {
+            $files = explode("\n", $files);
+            foreach ($files as $file) {
+                if ( ! empty($file)) {
+                    $fileEntry    = new ActivityEntry($file);
+                    $filesArray[] = $fileEntry->toArray();
+                }
+            }
+        }
+
+        return $filesArray;
+    }
+
+    /**
+     * @param array<string, list<array<string, string>>> $activity
+     * @return list<array<string, string>>
+     */
+    public function flattenActivity(array $activity): array
+    {
+        $result = array();
+        foreach ($activity as $disk => $files) {
+            foreach ($files as $file) {
+                $file['disk'] = $disk;
+                $result[]     = $file;
+            }
+        }
+        return $result;
+    }
+}
