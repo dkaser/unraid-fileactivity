f6269f9aee9a8d09d1a97f72df597122f747d7c4
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-06-13 22:10:19 +0000

    fix: strengthen build script

--- fileactivity-watcher/build.sh
+++ fileactivity-watcher/build.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Build static linux/amd64 binary
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -o fileactivity-watcher
 
 mkdir -p ../src/usr/local/php/unraid-fileactivity/bin
-go build
 cp fileactivity-watcher ../src/usr/local/php/unraid-fileactivity/bin/
